1bdd8fea5b01c17c3259932ee95734a792bc8b0c
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-08-16 19:09:13 +0530

    Fix for 1 to n instead of old 0 to n-1

--- Day2/Day2.cpp
+++ Day2/Day2.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int missingNum(vector<int> &nums)
 {
     int n = nums.size();
-    int xor1 = 0, xor2 = 0;
+    int xor1 = 0, xor2 = n;
     for (int i = 0; i < n; i++)
     {
         xor1 ^= nums[i];
-        xor2 ^= i;
+        xor2 ^= i+1;
     }
-    xor2 ^= n;
+    xor2 ^= n+1;
     return xor1 ^ xor2;
 }
 int main()
 {
-    // vector<int> nums = {3, 0, 1, 2};
-    // vector<int> nums = {0};
-    vector<int> nums;
-    for (int i = 0; i < 100000; i++)
+    // vector<int> nums = {3, 5, 1, 2};
+    vector<int> nums = {1};
+    // vector<int> nums;
+    for (int i = 1; i < 100000; i++)
         nums.push_back(i);
     cout << missingNum(nums) << endl;
     return 0;
